3dfdad473d3ba23f736bfebbbc9f63a7d9dce420
Mone't Fulgham <monetfulgham@gmail.com>
2019-11-27 13:10:37 -0500

    Export schema for query use

--- model.js
+++ model.js
@@ -5,17 +5,27 @@ mongoose.Promise = global.Promise;
 
 const db = mongoose.connect(`mongodb://localhost:27017/product-inventory`);
 
+function toLower(input){
+    return input.toLowerCase();
+}
+
 const productSchema = mongoose.Schema({
-    PRODUCT_NAME: String,
+    PRODUCT_NAME: { type: String, set: toLower },
     WAREHOUSE_NO: Number,
     STOCK: [{
-        SKU: String,
+        SKU: { type: String, set: toLower },
         WAREHOUSE_NO: Number,
         QTY: Number
     }],
+
     UNSTOCK: [{
-        SKU: String,
+        SKU:{ type: String, set: toLower },
         WAREHOUSE_NO: Number,
         QTY: Number
     }]
+
 });
+
+const product = mongoose.model('Product', productSchema);
+
+module.exports(product);
